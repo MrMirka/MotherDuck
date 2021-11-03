15c234dc7e44882c4d1001efe4dbf2ac878dc7f0
MrMirka <aleshin.dev@gmail.com>
2021-11-03 17:10:44 +0300

    vertion 59

--- scripts.js
+++ scripts.js
@@ -31,7 +31,7 @@ let composer;
 const deltas = {
 	x: 0.0002,
 	y: 0.0004,
-	speed: 0.080
+	speed: 0.0080
 };
 
 let revertDuck = true;
@@ -64,7 +64,7 @@ window.addEventListener("touchstart", barkOpen, false);
 
 
 
-console.log('vertion 0.12.58');
+console.log('vertion 0.12.59');
 
 
 init();
