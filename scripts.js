0dffaca27f0860b9f3e729152847cc63ad13ac25
MrMirka <aleshin.dev@gmail.com>
2021-11-08 18:19:19 +0300

    vertion 23

--- scripts.js
+++ scripts.js
@@ -175,7 +175,6 @@ function init(){
 	window.addEventListener("touchmove",  env => {
 		positions.push(env.changedTouches[0].pageX / window.innerWidth);
 		panYTouch.push(env.changedTouches[0].pageY / window.innerHeight);
-		alert(env.changedTouches[0].pageX + " "+ env.changedTouches[0].pageY);
 		checkTurn();
 		
 		
@@ -509,7 +508,7 @@ function checkTurn(){
 		let moveY = ( panYTouch[panYTouch.length-1] - panYTouch[panYTouch.length-3] ) / (window.innerHeight / window.innerWidth); 
 		let moveX = positions[positions.length-1] - positions[positions.length-3];
 
-
+		alert(Math.abs(moveX) +" "+ Math.abs(moveY));
 		if(Math.abs(moveX) > Math.abs(moveY)) {
 			if(last > preLast){
 				revertDuck = true;
