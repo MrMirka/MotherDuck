ba182e9fb5209643db2699e32987960ec82baa69
MrMirka <aleshin.dev@gmail.com>
2021-11-08 15:35:50 +0300

    vertion 89

--- scripts.js
+++ scripts.js
@@ -70,7 +70,7 @@ window.addEventListener("touchstart", barkOpen, false);
 
 
 
-console.log('vertion 0.12.88');
+console.log('vertion 0.12.89');
 
 
 init();
@@ -140,7 +140,6 @@ function init(){
 
 	
 	control = new OrbitControls(camera, renderer.domElement);
-	/*
 	control.enableDamping = true;
 	control.enablePan = false;
 	control.enableZoom = false;
@@ -148,7 +147,6 @@ function init(){
 	control.maxAzimuthAngle = 0.9530985059023817;
 	control.maxPolarAngle = 1.1936065021656286;
 	control.minPolarAngle = 1.1936065021656286;
-	*/
 	
 	control.update();
 	
